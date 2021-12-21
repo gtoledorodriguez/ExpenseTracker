77b50f1c69d3197f46d59eb011ff40ec8aece085
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2021-12-20 22:49:23 -0600

    User, Account, Transaction controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'report/index'
+  get 'user/index'
+  get 'account/index'
+  get 'transaction/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'user#index'
 end
